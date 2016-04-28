a43a0fb97eb2bdf5e97926e2186b310cd461c914
Sam Henderson <hendersonsam456@gmail.com>
2016-04-28 23:34:04 +1000

    added validation to startup form

--- app/models/start_up.rb
+++ app/models/start_up.rb
@@ -1,2 +1,11 @@
 class StartUp < ActiveRecord::Base
+  validates :name, presence: true
+  validates :short_description, presence: true
+  validates :long_description, presence: true
+  validates :image, presence: true
+  validates :video, presence: true
+  validates :stage_id, presence: true
+  validates :incubator_id, presence: true
+  validates :location_id, presence: true
+
 end
